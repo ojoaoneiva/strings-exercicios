ce027f06465dfd5de4b81292c494aa0a2b9e309a
João Neiva <jg.neiva@gmail.com>
2023-03-21 18:55:56 -0300

    resolução

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const nome = prompt("Qual o seu nome?")
+const comida1 = prompt("Qual a sua comida favorita?")
+const comida2 = prompt("Qual a sua segunda comida favorita")
+const comida3 = prompt("Qual a sua terceira comida favorita")
+
+console.log(`Estas são as comidas favoritas de ${nome}:
+- ${comida1} 
+- ${comida2}
+- ${comida3}`)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
-//Crie a const para a frase aqui
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+
+const novaFrase = frase.replace("verde","rosa").replace("azul","laranja")
+
+console.log(novaFrase.includes("verde").includes("laranja"))
+console.log(novaFrase.includes("laranja"))
+
+const maiusculo = "mas não deixe o gato sair".toUpperCase()
+const fraseFinal = novaFrase.replace("mas não deixe o gato sair", maiusculo)
+console.log(fraseFinal)
